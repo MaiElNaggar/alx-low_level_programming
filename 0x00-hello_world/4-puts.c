c118007f2570771b42768681c332b1fafece8f7b
MaiElNaggar <eng.maimohamed@gmail.com>
2023-06-15 13:35:56 -0700

    print using put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description : prints text in using puts function
+ * Return : 0 if exited properly
+ */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
