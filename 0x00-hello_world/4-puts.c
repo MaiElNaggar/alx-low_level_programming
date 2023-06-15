e37656ca03b0f9a818fc73750f5aa35b8d0fca75
MaiElNaggar <eng.maimohamed@gmail.com>
2023-06-15 13:49:08 -0700

    betty changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- * Description : prints text in using puts function
- * Return : 0 if exited properly
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
